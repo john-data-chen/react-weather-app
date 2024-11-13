64da6984ec9222de1ce235558261ca03a357a208
John Chen <john.data.chen@gmail.com>
2024-11-13 11:44:22 +0800

    temp commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import React from "react";
 
 function App() {
-  return <></>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Weather App</h1>
+      </header>
+    </div>
+  );
 }
 
 export default App;
